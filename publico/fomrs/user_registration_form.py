a33e4c0a124a7f1a82d5d7f4129695ef16fc05a1
NahuelGUERCI <nahuelguerci@gmail.com>
2023-05-02 20:10:00 -0300

    con form

--- publico/fomrs/user_registration_form.py
+++ publico/fomrs/user_registration_form.py
@@ -21,8 +21,10 @@ class ProfileEditForm(forms.Form):
     firstName = forms.CharField(max_length=100, validators=(solo_caracteres,),widget=forms.TextInput(attrs={'class': 'form-control'}))
     lastName = forms.CharField(max_length=100, validators=(solo_caracteres,),widget=forms.TextInput(attrs={'class': 'form-control'}))
     about = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
-    company = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    job = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    company_choices = [(1, 'Estudio'), (2, 'Trabajo'), (3, 'No trabajo ni estudio')]
+    company = forms.ChoiceField(choices=company_choices, widget=forms.Select(attrs={'class': 'form-control'}))
+    job_choices = [(1, 'Para el trabajo'), (2, 'Para mí'), (3, 'Para cambiar de trabajo')]
+    job = forms.ChoiceField(choices=job_choices, widget=forms.Select(attrs={'class': 'form-control'}))
     country = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     address = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     phone = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
@@ -30,6 +32,13 @@ class ProfileEditForm(forms.Form):
     twitter = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     facebook = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
    
+    def clean_job(self):
+        job_value = self.cleaned_data['job']
+        valid_choices = [choice[0] for choice in self.fields['job'].choices]
+        if int(job_value) not in valid_choices:
+            raise forms.ValidationError('Opción de trabajo inválida')
+        return job_value
+
     def clean(self):
         cleaned_data = super().clean()
         required_fields = ['username', 'firstName', 'lastName', 'about', 'company', 'job', 'country', 'address', 'phone', 'email']
@@ -38,3 +47,4 @@ class ProfileEditForm(forms.Form):
                 raise forms.ValidationError('Por favor completa todos los campos requeridos.')
 
         return cleaned_data
+
