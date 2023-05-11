42b9f0fc2627199b871fc54dd2c2239ce385be01
NahuelGUERCI <nahuelguerci@gmail.com>
2023-05-11 13:55:23 -0300

    cambio inicio de pag

--- django_inscripciones/urls.py
+++ django_inscripciones/urls.py
@@ -18,11 +18,13 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('publico/',include('publico.urls')),
-    path('cursos/',include('gestion_personas.urls')),
-    path('gestion/',include('gestion_cursos.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', RedirectView.as_view(url='/publico/')),
+    path('publico/', include('publico.urls')),
+    path('cursos/', include('gestion_personas.urls')),
+    path('gestion/', include('gestion_cursos.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
